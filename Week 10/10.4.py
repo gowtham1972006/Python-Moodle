74bba15791d167997e5787bc51dc01d3d569a910
gowtham1972006 <231001050@rajalakshmi.edu.in>
2024-06-15 00:37:28 +0530

    Create 10.4.py

---
+++ Week 10/10.4.py
@@ -0,0 +1,49 @@
+Write a Python program to sort a list of elements using the merge sort algorithm.
+
+Input	
+5
+6 5 4 3 8
+Result
+3 4 5 6 8
+
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+def main():
+    n = int(input())
+    elements = list(map(int, input().split()))
+
+    merge_sort(elements)
+    
+    print(" ".join(map(str, elements)))
+
+main()
