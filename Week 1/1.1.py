29164da7e68b6a720e83ae79ebfe4c2b5d226c25
gowtham1972006 <231001050@rajalakshmi.edu.in>
2024-06-14 21:29:13 +0530

    Create 1.1.py

---
+++ Week 1/1.1.py
@@ -0,0 +1,16 @@
+Write a program to convert strings to an integer and float and display its type.
+
+Sample Input:
+10
+10.9
+Sample Output:
+10,<class 'int'>
+10.9,<class 'float'>
+
+a=input()
+b=input()
+c=int(a)
+d=float(b)
+d=round(d,1)
+print(a,type(c),sep=",")
+print(d,type(d),sep=",")
